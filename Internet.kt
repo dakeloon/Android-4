5bcbb4962ffd7e38863b6798bbb3a15cf763eb01
dakeloon <94038117+dakeloon@users.noreply.github.com>
2022-12-08 11:57:13 +0700

    Create Internet.kt

---
+++ Internet.kt
@@ -0,0 +1,54 @@
+package com.example.internettest
+
+import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bitton: Button = findViewById(R.id.btnHTTP)
+        val button: Button = findViewById(R.id.btnOkHTTP)
+
+        val url = URL("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+
+        bitton.setOnClickListener {
+            Thread {
+                val connection = url.openConnection() as HttpURLConnection
+                val data = connection.inputStream.bufferedReader().readText()
+                runOnUiThread {
+                    Log.d("Flickr cats", data)
+                }
+            }.start()
+        }
+
+        button.setOnClickListener {
+            val okHttpClient = OkHttpClient()
+
+            val request: Request = Request.Builder().url(url).build()
+            okHttpClient.newCall(request).enqueue(object: Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                }
+                override fun onResponse(call: Call, response: Response) {
+                    val json = response.body()?.string()
+                    val txt = JSONObject(json.toString())
+
+                    runOnUiThread {
+                        Log.i("Flickr OkCats", txt.toString())
+                    }
+                }
+            })
+        }
+    }
+}
